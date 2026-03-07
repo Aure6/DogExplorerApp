efedd6524ce49411ab1db2206dcefe0ab0ac79f7
Aure6 <aurelien7112001@gmail.com>
2026-03-07 11:02:20 +0100

    Updated layout and styles, and modified Favorites page

--- DogExplorerApp/Models/FavoriteDog.cs
+++ DogExplorerApp/Models/FavoriteDog.cs
@@ -4,6 +4,6 @@ public class FavoriteDog
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string ImageUrl { get; set; }
-    public string CustomName { get; set; } = "Mon Chien Favori";
+    public string CustomName { get; set; } = "Mon chien favori";
     public string Description { get; set; } = "Ajoutez une description...";
 }
